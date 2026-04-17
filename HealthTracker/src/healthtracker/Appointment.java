4a3aa799a93dedebf67b31777175f57e485e3a0e
Dinh <quocdinh86@gmail.com>
2026-04-16 21:08:45 -0700

    First time push proj

---
+++ HealthTracker/src/healthtracker/Appointment.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package healthtracker;
+
+/**Appointment
+ * --------------------
+ * - doctor: Doctor
+ * - date: Date
+ * - time: Time
+ * --------------------
+ * Appointment(doctor:Doctor, date:Date, time:Time)
+ * +setDoctor(doc:Doctor)
+ * +setDate(d:Date)
+ * +setTime(t:Time)
+ * +getDoctor():Doctor
+ * +getDate():Date
+ * +getTime():Time
+ * +toString():String
+ * 
+ *
+ * @author dinhtran
+ */
+public class Appointment {
+    private Doctor doctor;
+    private Date date;
+    private Time time;
+    Appointment(Doctor doc, Date d, Time t){
+        doctor = doc;
+        date = d;
+        time = t;
+    }
+    
+    public void setDoctor(Doctor doc){
+        doctor = doc;
+    }
+    
+    public void setDate(Date d){
+        date = d;
+    }
+    
+    public void setTime(Time t){
+        time = t;
+    }
+    
+    public Doctor getDoctor(){
+        return doctor;
+    }
+    
+    public Date getDate(){
+        return date;
+    }
+    
+    public Time getTime(){
+        return time;
+    }
+    
+    @Override
+    public String toString(){
+        return String.format("%s\n%s \n%d/%d/%d \n%d:%d", doctor.getName(), doctor.getAddress(), date.getDay(), date.getMonth(), date.getYear(), time.getHour(), time.getMinute());
+    }
+}
